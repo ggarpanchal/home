9d2946132dddbe6b4a869e1ef4e80e26f6f0bfca
ggarpanchal <panchal.jigar90@gmail.com>
2020-08-03 00:49:04 +0530

    Updates

---
+++ precache-manifest.974e0408fb38ca9385f627ce8de98dbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3c42ad127e53ae03f776038526f67d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18ee693fe33acf9aeb4f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0037ab0155fb97fc55de",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "18ee693fe33acf9aeb4f",
+    "url": "/static/js/2.11047eb6.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.11047eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0037ab0155fb97fc55de",
+    "url": "/static/js/main.cedde166.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "a173bff9534edcfcd704ae969deccb98",
+    "url": "/static/media/resume.a173bff9.pdf"
+  }
+]);
